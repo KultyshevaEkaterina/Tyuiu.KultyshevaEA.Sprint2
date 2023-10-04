43c58d0ef2bc67576f4fa4453598bed1020492c0
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-10-04 21:14:12 +0500

    Task1

---
+++ Tyuiu.KultyshevaEA.Sprint2.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KultyshevaEA.Sprint2.Task1.V4.Lib
+{
+    public class DataService : ISprint2Task1V4
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c < b);
+            res[1] = (a * 2 > c) & (b != d);
+            res[2] = (c != d) || (b < a);
+            res[3] = (d * 4 <= a) && (b >= c);
+            res[4] = !(a == b);
+            res[5] = (b + 25 > a) ^ (d - 14 < c);
+
+            return res;
+        }
+    }
+}
